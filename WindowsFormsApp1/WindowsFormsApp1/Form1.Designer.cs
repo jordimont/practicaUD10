444ef91468c28df2093138b2745a0a3557313c9f
jmontalvoblanes <jmontalvoblanes@gmail.com>
2022-06-10 17:04:44 +0200

    3
    2

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -63,9 +63,9 @@ namespace WindowsFormsApp1
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(466, 46);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.Size = new System.Drawing.Size(32, 13);
             this.label3.TabIndex = 2;
-            this.label3.Text = "Telefono";
+            this.label3.Text = "Movil";
             // 
             // tNombre
             // 
